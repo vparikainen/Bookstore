4564f8e7297bf8b7823ffd6a824fb664c1212486
Vilma Parikainen <bgz832@myy.haaga-helia.fi>
2023-09-13 16:04:15 +0300

    Lisäsin welcome-sivun

--- src/main/java/hh/sof03/bookstore/web/BookController.java
+++ src/main/java/hh/sof03/bookstore/web/BookController.java
@@ -1,17 +1,24 @@
 package hh.sof03.bookstore.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import hh.sof03.bookstore.domain.Book;
+
 @Controller
 public class BookController {
 	
+	private final List<Book> books = new ArrayList<Book>();
+	
 	@RequestMapping(value="/index", method = RequestMethod.GET)
 	public String getBooks(Model model) {
-		
-	return "";	
+		model.addAttribute("car", new Book());
+		model.addAttribute("books", books);
+		return "welcome";	
 	}
-
 }
