03e9c71273d44e57b0618f3093571448d2e71755
Vilma Parikainen <bgz832@myy.haaga-helia.fi>
2023-09-20 21:01:58 +0300

    Loin delete ja add -toiminnot sekä add- ja save-sivujen templatet

--- src/main/java/hh/sof03/bookstore/web/BookController.java
+++ src/main/java/hh/sof03/bookstore/web/BookController.java
@@ -34,5 +34,21 @@ public class BookController {
 		return "booklist";
 	}
 	
-
+	@RequestMapping(value="add")
+	public String addBook(Model model) {
+		model.addAttribute("book", new Book());
+		return "addBook";
+	}
+	
+	@RequestMapping(value="/save", method = RequestMethod.POST)
+	public String save(Book book) {
+		repository.save(book);
+		return "redirect:booklist";
+	}
+	
+	@RequestMapping(value="/delete/{id}", method = RequestMethod.GET)
+	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+		repository.deleteById(bookId);
+		return "redirect:/booklist";
+	}
 }
