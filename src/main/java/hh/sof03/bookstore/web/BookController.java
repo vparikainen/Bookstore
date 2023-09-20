aa5ed97a6ee5712ee1a7e9ec35fa6f244bd9279b
Vilma Parikainen <bgz832@myy.haaga-helia.fi>
2023-09-20 21:26:16 +0300

    Lisäsin edit-toiminnon ja sille oman sivun

--- src/main/java/hh/sof03/bookstore/web/BookController.java
+++ src/main/java/hh/sof03/bookstore/web/BookController.java
@@ -51,4 +51,10 @@ public class BookController {
 		repository.deleteById(bookId);
 		return "redirect:/booklist";
 	}
+	
+	@RequestMapping(value="edit/{id}")
+	public String editBook(@PathVariable("id") Long bookId, Model model) {
+		model.addAttribute("book", repository.findById(bookId));
+		return "editbook";
+	}
 }
